82119394ac8b36d1d9a8e7d370b45993988feab8
nestorpirc-prog <nestorpirc@gmail.com>
2026-03-11 18:28:33 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,40 @@
+let money = 0
+let income = 1
 
+function update(){
+document.getElementById("money").innerText = "$" + money
+}
+
+function work(){
+money += income
+update()
+}
+
+function buyShop(){
+
+if(money >= 50){
+money -= 50
+income += 5
+alert("You bought a shop!")
+}
+
+update()
+
+}
+
+function buyCompany(){
+
+if(money >= 500){
+money -= 500
+income += 50
+alert("You bought a company!")
+}
+
+update()
+
+}
+
+setInterval(()=>{
+money += income
+update()
+},3000)
